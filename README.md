aa5f8b0ad6f05e92f7d8bc376cb9fd70ae8bd78e
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2018-10-20 23:41:41 -0400

    Export rdflib NamedNodes

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# solid-namespace
+# Solid Namespace
 [![](https://img.shields.io/badge/project-Solid-7C4DFF.svg?style=flat)](https://github.com/solid/solid)
 [![NPM Version](https://img.shields.io/npm/v/solid-namespace.svg?style=flat)](https://npm.im/solid-namespace)
 
 A collection of common RDF namespaces used in the Solid project.
 
+solid-namespace is meant to be used with [rdflib.js](/linkeddata/rdflib.js). It exports a collection of `NamedNode` values for commonly used namespaces, thereby shortcutting the need to call `$rdf.namespace(...)`.
+
 ## Usage
 
 ```js
-var rdf = require('rdflib')  // optional
-var vocab = require('solid-namespace')(rdf)  // or require('solid-namespace')()
-console.log(vocab.foaf('name'))  
-// -> NamedNode(<http://xmlns.com/foaf/0.1/name>)
+const $rdf = require('rdflib');
+const ns = require('solid-namespace');
+const store = $rdf.graph();
+    
+let me = ...;
+let name = store.any(me, ns.vcard(‘fn’)) || store.any(me, ns.foaf(‘name’));
+
+console.log(ns.foaf); // -> NamedNode(<http://xmlns.com/foaf/0.1/name>)
 ```
--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-'use strict'
 /**
- * Provides a hashmap of relevant vocabs / namespaces.
+ * Provides a way to access commonly used namespaces
+ *
  * Usage:
  *
  *   ```
- *   var rdf = require('rdflib')  // optional
- *   var vocab = require('solid-vocab')(rdf)  // or require('solid-vocab')()
- *   console.log(vocab.foaf('name'))  // -> <http://xmlns.com/foaf/0.1/name>
+ *   const $rdf = require('rdflib');
+ *   const ns = require('solid-namespace');
+ *   const store = $rdf.graph();
+ *
+ *   let me = ...;
+ *   let name = store.any(me, ns.vcard(‘fn’)) || store.any(me, ns.foaf(‘name’));
  *   ```
  * @module vocab
  */
 
-/**
- * @param [rdf] {RDF} Optional RDF Library (such as rdflib.js or rdf-ext) to
- *   inject
- */
-function vocab (rdf) {
-  var ns = require('rdf-ns')(rdf)
-  var vocabMap = {
-    'acl': ns.base('http://www.w3.org/ns/auth/acl#'),
-    'app': ns.base('http://www.w3.org/ns/solid/app#'),
-    'cert': ns.base('http://www.w3.org/ns/auth/cert#'),
-    'dct': ns.base('http://purl.org/dc/terms/'),
-    'foaf': ns.base('http://xmlns.com/foaf/0.1/'),
-    'ldp': ns.base('http://www.w3.org/ns/ldp#'),
-    'owl': ns.base('http://www.w3.org/2002/07/owl#'),
-    'pim': ns.base('http://www.w3.org/ns/pim/space#'),
-    'rdf': ns.base('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
-    'rdfs': ns.base('http://www.w3.org/2000/01/rdf-schema#'),
-    'schema': ns.base('http://schema.org/'),
-    'sioc': ns.base('http://rdfs.org/sioc/ns#'),
-    'solid': ns.base('http://www.w3.org/ns/solid/terms#'),
-    'stat': ns.base('http://www.w3.org/ns/posix/stat#'),
-    'vcard': ns.base('http://www.w3.org/2006/vcard/ns#'),
-    'xsd': ns.base('http://www.w3.org/2001/XMLSchema#')
-  }
-  return vocabMap
+var $rdf = require('rdflib')
+
+var vocab = {
+  acl: $rdf.Namespace('http://www.w3.org/ns/auth/acl#'),
+  arg: $rdf.Namespace('http://www.w3.org/ns/pim/arg#'),
+  cal: $rdf.Namespace('http://www.w3.org/2002/12/cal/ical#'),
+  contact: $rdf.Namespace('http://www.w3.org/2000/10/swap/pim/contact#'),
+  dc: $rdf.Namespace('http://purl.org/dc/elements/1.1/'),
+  dct: $rdf.Namespace('http://purl.org/dc/terms/'),
+  doap: $rdf.Namespace('http://usefulinc.com/ns/doap#'),
+  foaf: $rdf.Namespace('http://xmlns.com/foaf/0.1/'),
+  http: $rdf.Namespace('http://www.w3.org/2007/ont/http#'),
+  httph: $rdf.Namespace('http://www.w3.org/2007/ont/httph#'),
+  icalTZ: $rdf.Namespace('http://www.w3.org/2002/12/cal/icaltzd#'), // Beware: not cal:
+  ldp: $rdf.Namespace('http://www.w3.org/ns/ldp#'),
+  link: $rdf.Namespace('http://www.w3.org/2007/ont/link#'),
+  log: $rdf.Namespace('http://www.w3.org/2000/10/swap/log#'),
+  meeting: $rdf.Namespace('http://www.w3.org/ns/pim/meeting#'),
+  mo: $rdf.Namespace('http://purl.org/ontology/mo/'),
+  owl: $rdf.Namespace('http://www.w3.org/2002/07/owl#'),
+  pad: $rdf.Namespace('http://www.w3.org/ns/pim/pad#'),
+  patch: $rdf.Namespace('http://www.w3.org/ns/pim/patch#'),
+  qu: $rdf.Namespace('http://www.w3.org/2000/10/swap/pim/qif#'),
+  trip: $rdf.Namespace('http://www.w3.org/ns/pim/trip#'),
+  rdf: $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+  rdfs: $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#'),
+  rss: $rdf.Namespace('http://purl.org/rss/1.0/'),
+  sched: $rdf.Namespace('http://www.w3.org/ns/pim/schedule#'),
+  schema: $rdf.Namespace('http:/schema.org/'), // @@ beware confusion with documents no 303
+  sioc: $rdf.Namespace('http://rdfs.org/sioc/ns#'),
+  solid: $rdf.Namespace('http://www.w3.org/ns/solid/terms#'),
+  space: $rdf.Namespace('http://www.w3.org/ns/pim/space#'),
+  stat: $rdf.Namespace('http://www.w3.org/ns/posix/stat#'),
+  tab: $rdf.Namespace('http://www.w3.org/2007/ont/link#'),
+  tabont: $rdf.Namespace('http://www.w3.org/2007/ont/link#'),
+  ui: $rdf.Namespace('http://www.w3.org/ns/ui#'),
+  vcard: $rdf.Namespace('http://www.w3.org/2006/vcard/ns#'),
+  wf: $rdf.Namespace('http://www.w3.org/2005/01/wf/flow#'),
+  xsd: $rdf.Namespace('http://www.w3.org/2001/XMLSchema#')
 }
 
 module.exports = vocab
