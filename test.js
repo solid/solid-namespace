07703d1441ae9bfca8f1302199661e571cfd3ecd
Michiel de Jong <michiel@unhosted.org>
2020-07-30 12:18:41 +0200

    Fix tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 //Test the non-rdflib functionality
 const ns = require('./index.js')();
-assert.equal(ns.schema('Recipe'), 'http:/schema.org/Recipe');
+assert.equal(ns.schema('Recipe'), 'http://schema.org/Recipe');
 
 
 //Test the rdflib functionality
@@ -10,4 +10,4 @@ const rdflib = {
     namedNode: val => 'RDF::'+val
 };
 const rdfns = require('./index.js')(rdflib);
-assert.equal(rdfns.schema('Recipe'), 'RDF::http:/schema.org/Recipe');
+assert.equal(rdfns.schema('Recipe'), 'RDF::http://schema.org/Recipe');
